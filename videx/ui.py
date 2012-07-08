49f571d86b131f288af8e22bb63c3e9fe83f5b7b
crccheck <c@crccheck.com>
2012-07-08 11:16:47 -0500

    get the app to accept drag and drop of files

--- videx/ui.py
+++ videx/ui.py
@@ -6,9 +6,24 @@ APP_TITLE = 'videx'
 
 
 class AppWindow(QtGui.QWidget):
-    def __init__(self):
-        super(AppWindow, self).__init__()
+    def __init__(self, *args, **kwargs):
+        super(AppWindow, self).__init__(*args, **kwargs)
+        self.setAcceptDrops(True)
         self.initUI()
+
+    def dragEnterEvent(self, evt):
+        if evt.mimeData().hasUrls:
+            evt.accept()
+        else:
+            evt.ignore()
+
+    def dropEvent(self, evt):
+        if evt.mimeData().hasUrls:
+            links = [x.toLocalFile() for x in evt.mimeData().urls()]
+            print links
+            evt.accept()
+        else:
+            evt.ignore()
 
     def initUI(self):
         self.setWindowTitle(APP_TITLE)
@@ -18,6 +33,7 @@ class AppWindow(QtGui.QWidget):
 def main():
     app = QtGui.QApplication(sys.argv)
     app_window = AppWindow()
+    print app_window
     sys.exit(app.exec_())
 
 
