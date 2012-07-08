90fac09d327ab1290772b516fe79253f54902615
crccheck <c@crccheck.com>
2012-07-08 16:35:42 -0500

    make sure file paths are unique

--- omnmeta/tests/models.py
+++ omnmeta/tests/models.py
@@ -1,5 +1,7 @@
+from nose.tools import raises
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
 
 from omnmeta import settings
 
@@ -21,3 +23,11 @@ class TestFileModel:
         self.session.add(f)
         self.session.commit()
         assert self.session.query(File).count() == 1
+
+    @raises(IntegrityError)
+    def test_make_sure_path_is_unique(self):
+        f = File(path='/path/to/some/file')
+        self.session.add(f)
+        f = File(path='/path/to/some/file')
+        self.session.add(f)
+        self.session.commit()
