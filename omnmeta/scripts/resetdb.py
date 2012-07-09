9056c40893957285f5b1c1d3b39b68666370ec06
crccheck <c@crccheck.com>
2012-07-08 23:34:58 -0500

    fix: reset script didn't work because it didn't delete old db

--- omnmeta/scripts/resetdb.py
+++ omnmeta/scripts/resetdb.py
@@ -1,10 +1,19 @@
 #! /usr/bin/env python
 from _fixpath import *
 
+import os
+
 from sqlalchemy import create_engine
 
 from omnmeta import settings
+from omnmeta.settings import project_dir
 from omnmeta.models import Base
 
+# delete old database
+db_path = project_dir(settings.DB_FILENAME)
+if os.path.isfile(db_path):
+    os.unlink(db_path)
+
+# create new database
 engine = create_engine(settings.DB, echo=True)
 Base.metadata.create_all(engine)
--- omnmeta/settings.py
+++ omnmeta/settings.py
@@ -1,7 +1,9 @@
 import os
 
+
 def project_dir(*path):
     base = os.path.realpath(os.path.dirname(__file__))
     return os.path.join(base, *path)
 
-DB = 'sqlite:///%s' % project_dir('database.sqlite')
+DB_FILENAME = 'database.sqlite'
+DB = 'sqlite:///%s' % project_dir(DB_FILENAME)
