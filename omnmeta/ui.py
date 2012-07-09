6caf860be651850a204013461b56995447dd042b
crccheck <c@crccheck.com>
2012-07-08 23:37:01 -0500

    update view to display the name

--- omnmeta/ui.py
+++ omnmeta/ui.py
@@ -11,7 +11,10 @@ class ListWidget(QtGui.QListWidget):
     def __init__(self, *args, **kwargs):
         super(ListWidget, self).__init__(*args, **kwargs)
         for f in library.get():
-            self.addItem("%s" % f)
+            self.addItem(f)
+
+    def addItem(self, obj):
+        super(ListWidget, self).addItem("{0.name} - {0.path}".format(obj))
 
 
 class AppWindow(QtGui.QWidget):
@@ -32,7 +35,7 @@ class AppWindow(QtGui.QWidget):
             for link in links:
                 obj, created = library.add(link)
                 if created:
-                    self.main_widget.addItem(str(obj))
+                    self.main_widget.addItem(obj)
                 # from pdb4qt import set_trace; set_trace()
             evt.accept()
         else:
